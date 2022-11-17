2c363346ff5011ca0f975ba2f542e7b87b5b7275
egor-fan <juventini109@gmail.com>
2022-11-17 18:22:42 +0300

    burger scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
   $('.nav_burger').click(function(event){
-    $('.nav_burger, .menu_burger').toggleClass('active');
-    $('body').toggleClass('lock')
-    
+    $('.menu_burger').toggleClass('active');
+    $('body,html').toggleClass('lock');
   })
 })
+
+$(document).ready(function(){
+  $('.nav_burger').click(function(event) {
+    $('.menu__burger').removeClass('active');
+  })
+})
+
 
